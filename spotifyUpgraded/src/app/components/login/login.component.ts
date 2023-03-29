58137e837b936c08196f8b7849aefd71cfcf2ea0
sebC18 <capdeville.sebastien@gmail.com>
2023-03-29 18:54:45 +0200

    Fix redirectURL page login + retrieve login token after connection

--- spotifyUpgraded/src/app/components/login/login.component.ts
+++ spotifyUpgraded/src/app/components/login/login.component.ts
@@ -11,12 +11,15 @@ export class LoginComponent implements OnInit {
   constructor(private spotifyService: SpotifyService) { }
 
   ngOnInit(): void {
+   this.checkTokenCallbackUrl();
+  }
+
+  checkTokenCallbackUrl(){
+    const token =  this.spotifyService.retrieveTokenCallbackUrl();
   }
 
   OpenLoginPage() {
     window.location.href =  this.spotifyService.retrieveLoginURL();
-    
-    //alert('Ceci est un test du clic');
   }
 
 }
